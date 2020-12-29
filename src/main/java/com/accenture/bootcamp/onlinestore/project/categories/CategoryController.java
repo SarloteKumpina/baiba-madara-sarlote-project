e5a273793709f11e1be2e806f1b8fba507a8e1ec
SarloteKumpina <sarlote.kumpina@gmil.com>
2020-12-29 17:15:46 +0200

    added thymeleaf in all html files, added layout folder for head and navigation (replaced in cms html files with thymeleaf)

--- src/main/java/com/accenture/bootcamp/onlinestore/project/categories/CategoryController.java
+++ src/main/java/com/accenture/bootcamp/onlinestore/project/categories/CategoryController.java
@@ -1,48 +1,54 @@
 package com.accenture.bootcamp.onlinestore.project.categories;
 
-import com.accenture.bootcamp.onlinestore.project.products.ProductRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
-@RequestMapping("/online-shop/category")
-@RestController
+@Controller
 public class CategoryController {
 
     private final CategoryRepository categoryRepository;
-    private final ProductRepository productRepository;
 
     @Autowired
-    public CategoryController(CategoryRepository repository, ProductRepository productRepository) {
+    public CategoryController(CategoryRepository repository) {
         this.categoryRepository = repository;
-        this.productRepository = productRepository;
     }
 
-    @GetMapping("/{id}")
-    public Category findOne(@PathVariable long id) {
-        return categoryRepository.findOne(id);
+    @GetMapping(path = {"/admin/categories", "/admin"})
+    public String getAllCategories(Model model){
+        List<Category> allCategories = categoryRepository.findAll();
+        model.addAttribute("allCategories", allCategories);
+        return "cms/categories/categories";
     }
 
-    @GetMapping
-    public List<Category> getCategories() {
-        return categoryRepository.findAll();
-    }
-
-    //todo do not allow to add, if category by name already exists
-    @PostMapping
-    public Category create(@RequestBody CategoryRequest category) {
-        return categoryRepository.insert(category);
-    }
-
-    @PutMapping("/{id}")
-    public Category update(@PathVariable long id,
-                           @RequestBody CategoryRequest category) {
-        return categoryRepository.update(id, category);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable long id) {
-        categoryRepository.delete(id);
-    }
 }
+
+//    @GetMapping("/{id}")
+//    public Category findOne(@PathVariable long id) {
+//        return categoryRepository.findOne(id);
+//    }
+//
+//    @GetMapping
+//    public List<Category> getCategories() {
+//        return categoryRepository.findAll();
+//    }
+//
+//    //todo do not allow to add, if category by name already exists
+//    @PostMapping
+//    public Category create(@RequestBody CategoryRequest category) {
+//        return categoryRepository.insert(category);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public Category update(@PathVariable long id,
+//                           @RequestBody CategoryRequest category) {
+//        return categoryRepository.update(id, category);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void delete(@PathVariable long id) {
+//        categoryRepository.delete(id);
+//    }
