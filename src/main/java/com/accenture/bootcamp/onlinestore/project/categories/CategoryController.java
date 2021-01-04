65a11a1e63cf20f860d3faeb6182f61d3ac6876e
SarloteKumpina <sarlote.kumpina@gmil.com>
2021-01-04 19:11:10 +0200

    edited CategoryController, categories.html, create-category.html, update-category.html

--- src/main/java/com/accenture/bootcamp/onlinestore/project/categories/CategoryController.java
+++ src/main/java/com/accenture/bootcamp/onlinestore/project/categories/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -23,60 +24,29 @@ public class CategoryController {
     }
 
     @GetMapping(path = {"/admin/categories", "/admin"})
-    public String getAllCategories(Model model){
+    public String getAllCategories(Model model) {
         List<Category> allCategories = categoryRepository.findAll();
         model.addAttribute("allCategories", allCategories);
         return "cms/categories/categories";
     }
 
-    @GetMapping("admin/categories/new")
-    public String displayCategoryForm(Category category){
-        return "cms/categories/create-edit-category";
+    @GetMapping("/admin/categories/update/{id}")
+    public String displayCategoryUpdateForm(@PathVariable("id") long id, Model model) {
+        Category categoryForUpdate = categoryRepository.findOne(id);
+        model.addAttribute("categoryForUpdate", categoryForUpdate);
+        return "cms/categories/update-category";
     }
 
-    @PostMapping("/admin/categories/new")
-    public String addNewCategory(@Valid CategoryRequest category, BindingResult result){
-        if (result.hasErrors()) {
-            return "categories/create-category";
-        } else {
-            categoryRepository.insert(category);
-            return "redirect:/admin/categories";
-        }
+    @PostMapping("/admin/categories/update/{id}")
+    public String updateCategory(@PathVariable("id") long id, Category category) {
+        categoryRepository.update(id, category);
+        return "redirect:/admin/categories";
     }
 
-
-
-//    @GetMapping(path = {"/admin/categories", "/admin"})
-//    public String getAllCategories(Model model){
-//        model.addAttribute("allCategories", categoryRepository.findAll());
-//        return "cms/categories/categories";
-//    }
+    @GetMapping("/admin/categories/delete/{id}")
+    public String deleteCategory(@PathVariable("id") long id) {
+        categoryRepository.delete(id);
+        return "redirect:/admin/categories";
+    }
 
 }
-
-//    @GetMapping("/{id}")
-//    public Category findOne(@PathVariable long id) {
-//        return categoryRepository.findOne(id);
-//    }
-//
-//    @GetMapping
-//    public List<Category> getCategories() {
-//        return categoryRepository.findAll();
-//    }
-//
-//    //todo do not allow to add, if category by name already exists
-//    @PostMapping
-//    public Category create(@RequestBody CategoryRequest category) {
-//        return categoryRepository.insert(category);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public Category update(@PathVariable long id,
-//                           @RequestBody CategoryRequest category) {
-//        return categoryRepository.update(id, category);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void delete(@PathVariable long id) {
-//        categoryRepository.delete(id);
-//    }
