7d98d31a95db6adc1b9789629f12a25ce1548517
ZigridaMalinovska <71457298+ZigridaMalinovska@users.noreply.github.com>
2021-01-22 23:27:59 +0200

    added possibility to change quantity of product in Shopping cart

--- src/main/java/com/accenture/bootcamp/onlinestore/project/shoppingcart/ShoppingCartController.java
+++ src/main/java/com/accenture/bootcamp/onlinestore/project/shoppingcart/ShoppingCartController.java
@@ -1,11 +1,11 @@
 package com.accenture.bootcamp.onlinestore.project.shoppingcart;
 
+import com.accenture.bootcamp.onlinestore.project.addtocart.AddToCartForm;
+import com.accenture.bootcamp.onlinestore.project.orderproduct.OrderProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import static com.accenture.bootcamp.onlinestore.project.cookies.Cookies.USER_ID_COOKIE_NAME;
@@ -14,27 +14,47 @@ import static com.accenture.bootcamp.onlinestore.project.cookies.Cookies.USER_ID
 public class ShoppingCartController {
 
     private final ShoppingCartRepository shoppingCartRepository;
+    private final OrderProductService orderProductService;
 
     @Autowired
-    public ShoppingCartController(ShoppingCartRepository shoppingCartRepository) {
+    public ShoppingCartController(ShoppingCartRepository shoppingCartRepository, OrderProductService orderProductService) {
         this.shoppingCartRepository = shoppingCartRepository;
+        this.orderProductService = orderProductService;
     }
 
     @GetMapping("/cart")
-    public String cart(@CookieValue(name = USER_ID_COOKIE_NAME, required = false) String userId,
+    public String getCart(@CookieValue(name = USER_ID_COOKIE_NAME, required = false) String userId,
                        Model model) {
         if (userId == null){
             return "shop/cart-empty";
         } else {
         Long orderId = shoppingCartRepository.getOrderIdByCookieAndStatusShoppingCart(userId);
-        List<ShoppingCart> products = shoppingCartRepository.getProductsForOrderStatusShoppingCart(orderId);
-        model.addAttribute("products", products);
+            AddToCartForm productForm = new AddToCartForm();
+            List<ShoppingCart> products = shoppingCartRepository.getProductsForOrderStatusShoppingCart(orderId);
+            model.addAttribute("products", products);
+            model.addAttribute("product", productForm);
         return "shop/cart";
         }
     }
 
+    @PostMapping("/cart/update/{productId}")
+    public String updateCart(@PathVariable Long productId,
+                             @RequestParam("quantity") int quantity,
+                             @CookieValue(name = USER_ID_COOKIE_NAME, required = false) String userId,
+                             Model model) {
+        Long orderId = shoppingCartRepository.getOrderIdByCookieAndStatusShoppingCart(userId);
+        if(orderProductService.userHasThisProductInCart(orderId, productId)){
+            int productQuantityInCart = orderProductService.getProductQuantityFromOrder(orderId, productId);
+            productQuantityInCart = quantity;
+            orderProductService.updateProductQuantityInOrder(productQuantityInCart, orderId, productId);
+        }
+        List<ShoppingCart> products = shoppingCartRepository.getProductsForOrderStatusShoppingCart(orderId);
+        model.addAttribute("products", products);
+        return "shop/cart";
+    }
+
     @GetMapping(path = {"/cart/{orderId}"})
-    public String ShoppingCart(@PathVariable(required = false) Long orderId, Model model) {
+    public String getShoppingCartsByOrder(@PathVariable(required = false) Long orderId, Model model) {
         if (orderId == null) {
             return "shop/cart-empty";
         } else if (orderId != null && shoppingCartRepository.getProductsForOrderStatusShoppingCart(orderId).isEmpty()){
@@ -46,18 +66,13 @@ public class ShoppingCartController {
         }
     }
 
-
-//URL in cart.html
-//<!--<a th:href="@{/cart/delete/{productId}(productId=${product.id})}">-->
     @GetMapping("/cart/delete/{productId}")
-    public String removeProductFromShoppingCart(@CookieValue(name = USER_ID_COOKIE_NAME, required = false) String userId,
+    public String removeProductFromCart(@CookieValue(name = USER_ID_COOKIE_NAME, required = false) String userId,
                                                 @PathVariable(required = true) Long productId) {
         if (userId == null) {
             return "shop/cart-empty";
         } else {
-//7. get order_id of order found in 6th point
             Long orderId = shoppingCartRepository.getOrderIdByCookieAndStatusShoppingCart(userId);
-//11. remove product from orders_products
             shoppingCartRepository.removeProductFromShoppingCart(productId, orderId);
             return "redirect:/cart";
         }
