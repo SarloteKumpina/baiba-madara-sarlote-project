3730391a02024c08bafc61161b0848514f725f3d
BaibaIvane <baibaiivaane@gmail.com>
2021-01-23 20:07:22 +0200

    Categories pictures

--- src/main/resources/db/migration/V3__InsertDataIntoCategoriesTable.sql
+++ src/main/resources/db/migration/V3__InsertDataIntoCategoriesTable.sql
@@ -3,6 +3,6 @@ VALUES
 (null, 'Jimmy Choo', 'https://i.pinimg.com/originals/1d/8e/a8/1d8ea811deb52a6929e895f64bc96ed5.jpg'),
 (null, 'Tom Ford', 'https://i.pinimg.com/originals/53/aa/d8/53aad883f8da7086db3ea43dfdadd9b2.jpg'),
 (null, 'Ted Baker', 'https://riyadh-park.com/wp-content/uploads/2019/10/ted-baker-london-logo1.jpeg'),
-(null, 'Shoes', 'https://www.coltortiboutique.com/media/catalog/product/cache/06764c7e561f871da5967ecc153c7d6c/p/u/pumps_jimmy-choo_misto_201516nde000001-bmix-3.jpg'),
-(null, 'Sunglasses', 'https://johnlewis.scene7.com/is/image/JohnLewis/237472354'),
-(null, 'Dresses', 'https://media.tedbaker.com/t_xld_pdp_zoom,q_auto:best,f_auto/Product/Womens/243836_BLACK_1');
+(null, 'Shoes', 'https://i.imgur.com/ErG0lJ6.jpg'),
+(null, 'Sunglasses', 'https://i.imgur.com/OcEd69t.jpg'),
+(null, 'Dresses', 'https://i.imgur.com/tSS7xzg.png');
