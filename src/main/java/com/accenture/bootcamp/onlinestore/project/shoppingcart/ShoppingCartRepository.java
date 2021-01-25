eedbcc10ca973c2df98e47178a1c322635335f84
ZigridaMalinovska <71457298+ZigridaMalinovska@users.noreply.github.com>
2021-01-25 22:51:54 +0200

    removed redundant

--- src/main/java/com/accenture/bootcamp/onlinestore/project/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/com/accenture/bootcamp/onlinestore/project/shoppingcart/ShoppingCartRepository.java
@@ -14,33 +14,28 @@ public class ShoppingCartRepository implements ShoppingCartMapper {
         this.shoppingCartMapper = shoppingCartMapper;
     }
 
-    //6. if customer is known, get order by cookie and order statuss is SHOPPING CART (=1) - laikam netiek izmantots
     public Order getOrderByCookieAndStatusShoppingCart(String userId, int statusId){
         Order order = shoppingCartMapper.getOrderByCookieAndStatusShoppingCart(userId, statusId);
         return order;
     }
 
-    //7. get order_id of order found in 6th point - laikam pārnests
     public Long getOrderIdByCookieAndStatusShoppingCart(String userId, int statusId){
         Long orderId = shoppingCartMapper.getOrderIdByCookieAndStatusShoppingCart(userId, statusId);
         return orderId;
     }
 
-    //3. get all orders_products of one order by order_id in SHOPPING CART (=1) status
     @Override
     public List<ShoppingCart> getProductsForOrderStatusShoppingCart(Long id, int statusId) {
         List<ShoppingCart> products = shoppingCartMapper.getProductsForOrderStatusShoppingCart(id, statusId);
         return products;
     }
 
-    //12. get all orders_products in SHOPPING CART (=1) status - OrdersProducts ir
     @Override
     public List<ShoppingCart> getProductsInStatusShoppingCart(int statusId){
         List<ShoppingCart> products = shoppingCartMapper.getProductsInStatusShoppingCart(statusId);
         return products;
     }
 
-    //11. remove product from orders_products
     @Override
     public void removeProductFromShoppingCart(Long productId, Long orderId) {
         shoppingCartMapper.removeProductFromShoppingCart(productId, orderId);
