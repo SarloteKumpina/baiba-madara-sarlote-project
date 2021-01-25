4bb332393c90c930deae63d7f6635e4fb9b5b050
ZigridaMalinovska <71457298+ZigridaMalinovska@users.noreply.github.com>
2021-01-25 22:52:30 +0200

    removed redundant

--- src/main/java/com/accenture/bootcamp/onlinestore/project/shoppingcart/ShoppingCartTableSql.java
+++ src/main/java/com/accenture/bootcamp/onlinestore/project/shoppingcart/ShoppingCartTableSql.java
@@ -2,15 +2,12 @@ package com.accenture.bootcamp.onlinestore.project.shoppingcart;
 
 public final class ShoppingCartTableSql {
 
-//    6. if customer is known, get order by cookie and order statuss is SHOPPING CART (=1) - Orders nav
     public static final String GET_ORDER_DETAILS_BY_USER_ID_AND_STATUS_ID =
         "select * from orders where user_id = #{userId} and status_id = #{statusId}";
 
-//    7. get order_id of order found in 6th point - Orders ir
     public static final String GET_ORDER_ID_BY_USER_ID_AND_STATUS_ID =
         "select id from orders where user_id = #{userId} and status_id = #{statusId}";
 
-//    3. get all orders_products of one order in SHOPPING CART (=1) status - OrdersProducts
     public static final String GET_ORDERS_PRODUCTS_BY_ORDER_ID_AND_STATUS_ID =
         "select products.imageUri, products.id, products.name, products.price, orders_products.quantity, " +
         "(products.price * orders_products.quantity) as total\n" +
@@ -20,7 +17,6 @@ public final class ShoppingCartTableSql {
         "inner join status on orders.status_id = status.id\n" +
         "where status.id = #{statusId} AND #{orderId} IS NULL OR (orders.id = #{orderId})";
 
-//    12. get all orders_products in SHOPPING CART (=1) status - OrdersProducts
     public static final String GET_ORDERS_PRODUCTS_BY_STATUS_ID =
         "select products.imageUri, products.id, " +
         "products.name, products.price, orders_products.quantity, " +
@@ -31,7 +27,6 @@ public final class ShoppingCartTableSql {
         "inner join status on orders.status_id = status.id\n" +
         "where status.id=#{statusId}";
 
-//    11. remove product from orders_products - OrdersProducts
     public static final String REMOVE_PRODUCT_FROM_ORDERS_PRODUCTS = "delete from orders_products " +
         "where product_id=#{productId} AND order_id = #{orderId}";
 }
